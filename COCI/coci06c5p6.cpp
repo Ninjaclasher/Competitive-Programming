e68ce1894818e8679e294b73c50d27e3cdc688bb
Evan <evanzhang1028@hotmail.com>
2020-02-26 19:45:44 -0500

    Update coci06c5p6

--- COCI/coci06c5p6.cpp
+++ COCI/coci06c5p6.cpp
@@ -3,55 +3,55 @@
 
 using namespace std;
 
-const int LENGTH = 200003;
+const int MAXN = 200005;
 
-long long base = 27;
+long long BASE1 = 31, MOD1 = 1000000007, BASE2 = 37, MOD2 = 1000000009;
 
-struct hsh
+vector<long long> h1(MAXN), p1(MAXN);
+vector<long long> h2(MAXN), p2(MAXN);
+
+static inline long long get1(int l, int r)
 {
-    vector<long long> hh, pb;
-    hsh(){}
-    hsh(string b)
-    {
-        int sz = b.size();
-        pb.resize(sz+1, 1);    
-        for (unsigned int x = 1; x < pb.size(); x++)
-            pb[x] = (pb[x-1]*base);
-        hh.resize(sz+1);
-        for (int x = 1; x <= sz; x++)
-            hh[x] = (hh[x-1]*base+b[x-1]);
-    }
-    inline long long get(int l, int r)
-    {
-        return (hh[r+1]-hh[l]*pb[r-l+1]);
-    }
-};
+    return ((h1[r+1] - h1[l] * p1[r -l + 1]) % MOD1 + MOD1) % MOD1;
+}
+
+static inline long long get2(int l, int r)
+{
+    return ((h2[r+1] - h2[l] * p2[r -l + 1]) % MOD2 + MOD2) % MOD2;
+}
 
 int n;
-unordered_set<long long> st;
-hsh h;
+
+long long hashes[MAXN];
 
 static inline bool test(int len)
 {
-    st.clear();
-    for (int x = 1; x <= n-len; x++)
-    {
-        long long v = h.get(x, x+len);
-        if (st.count(v))
+    int i = 0;
+    for (int x = 0; x < n-len; x++)
+        hashes[i++] = get1(x, x+len) << 32 | get2(x, x+len);
+    sort(hashes, hashes+i);
+    for (int x = 0; x < i-1; x++)
+        if (hashes[x] == hashes[x+1])
             return true;
-        st.insert(v);
-    }
     return false;
 }
 
 int main()
 {
+    p1[0] = 1;
+    p2[0] = 1;
+    for (int x = 1; x < MAXN; x++)
+        p1[x] = (p1[x-1] * BASE1) % MOD1,
+        p2[x] = (p2[x-1] * BASE2) % MOD2;
+
     cin.tie(0);
     cin.sync_with_stdio(0);
     cin>>n;
     string a;
     cin>>a;
-    h = hsh(a);
+    for (int x = 1; x <= a.length(); x++)
+        h1[x] = (h1[x-1] * BASE1 + a[x-1] - 'a' + 1) % MOD1,
+        h2[x] = (h2[x-1] * BASE2 + a[x-1] - 'a' + 1) % MOD2;
     int l = 0, r = n-1;
     while (l <= r)
     {
