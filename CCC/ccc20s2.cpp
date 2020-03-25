8e98713c404edfe1da35ef31aaa50cdf088dfd57
Evan <evanzhang1028@hotmail.com>
2020-03-25 16:23:03 -0400

    Update ccc20s2 to pass on DMOJ

--- CCC/ccc20s2.cpp
+++ CCC/ccc20s2.cpp
@@ -7,13 +7,20 @@ const int MAXN = 1005;
 bool vis[MAXN][MAXN];
 int grid[MAXN][MAXN];
 
+vector<vector<pair<int,int>>> adj(1000005);
+
 int main()
 {
     int m, n;
     scanf("%i%i", &m, &n);
     for (int x = 1; x <= m; x++)
+    {
         for (int y = 1; y <= n; y++)
+        {
             scanf("%i", &grid[x][y]);
+            adj[x * y].push_back({x, y});
+        }
+    }
     vis[1][1] = 1;
     queue<pair<int,int>> buf;
     buf.push({1, 1});
@@ -24,17 +31,11 @@ int main()
         if (x == m && y == n)
             return 0 * printf("yes\n");
         buf.pop();
-        for (int xx = 1; xx * xx <= grid[x][y]; xx++)
+        for (auto &v : adj[grid[x][y]])
         {
-            if (grid[x][y] % xx == 0)
-            {
-                int nx = xx, ny = grid[x][y] / xx;
-                if (nx <= m && ny <= n && !vis[nx][ny])
-                    buf.push({nx, ny}), vis[nx][ny] = 1;
-                swap(nx, ny);
-                if (nx <= m && ny <= n && !vis[nx][ny])
-                    buf.push({nx, ny}), vis[nx][ny] = 1;
-            }
+            int nx = v.first, ny = v.second;
+            if (nx <= m && ny <= n && !vis[nx][ny])
+                buf.push({nx, ny}), vis[nx][ny] = 1;
         }
     }
     printf("%s\n", vis[m][n] ? "yes" : "no");
