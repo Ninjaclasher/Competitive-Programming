ad011bb29cde46349eb5f738fd37890e222f2479
Evan <evanzhang1028@hotmail.com>
2020-07-29 10:09:12 -0400

    Fix ccoqr16p3 solution

--- CCC/ccoqr16p3.cpp
+++ CCC/ccoqr16p3.cpp
@@ -2,31 +2,30 @@
 
 using namespace std;
 
+long long f(long long x)
+{
+    return x * (x + 1) / 2;
+}
+
 int main()
 {
-    int n, m, r, c, prev = 0;
+    int n, m, r, c;
     scanf("%i%i", &n, &m);
-    long long final = 0;
     vector<pair<int,int>> block;
     while (m--)
     {
         scanf("%i%i", &r, &c);
-        block.push_back(make_pair(c,n-r+1));
+        block.push_back(make_pair(c, n - r + 1));
     }
+    block.push_back(make_pair(n + 1, 0));
     sort(block.begin(), block.end());
-    for (int x = 1, index = 0; x < n+1; x++)
+    int height = block[0].second;
+    long long ans = 0;
+    for (int x = 1; x < block.size(); x++)
     {
-        while (x == block[index].first)
-        {
-            prev = max(prev, block[index].second);
-            index++;
-        }
-        if (prev > 0)
-        {
-            final+=prev;
-            prev--;
-        }
+        int diff = block[x].first - block[x - 1].first;
+        ans += f(height) - f(max(height - diff, 0));
+        height = max(height - diff, block[x].second);
     }
-    printf("%lli", final);
-    return 0;
+    printf("%lli\n", ans);
 }
