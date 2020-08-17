0ddaa9dbf25f8844d18a271510d3924ecc0a54ba
Evan <evanzhang1028@hotmail.com>
2020-08-17 19:14:43 -0400

    Fix ccc02j1 solution

--- CCC/ccc02j1.cpp
+++ CCC/ccc02j1.cpp
@@ -10,8 +10,29 @@ int main() {
         case 0:
             cout <<" * * *\n*     *\n*     *\n*     *\n\n*     *\n*     *\n*     *\n * * *\n";
         break;
+        case 1:
+            cout <<"\n      *\n      *\n      *\n\n      *\n      *\n      *\n\n";
+        break;
+        case 2:
+            cout <<" * * *\n      *\n      *\n      *\n * * *\n*\n*\n*\n * * *\n";
+        break;
+        case 3:
+            cout <<" * * *\n      *\n      *\n      *\n * * *\n      *\n      *\n      *\n * * *\n";
+        break;
+        case 4:
+            cout <<"\n*     *\n*     *\n*     *\n * * *\n      *\n      *\n      *\n\n";
+        break;
         case 5:
             cout <<" * * *\n*\n*\n*\n * * *\n      *\n      *\n      *\n * * *\n";
+        break;
+        case 6:
+            cout <<" * * *\n*\n*\n*\n * * *\n*     *\n*     *\n*     *\n * * *\n";
+        break;
+        case 7:
+            cout <<" * * *\n      *\n      *\n      *\n\n      *\n      *\n      *\n\n";
+        break;
+        case 8:
+            cout <<" * * *\n*     *\n*     *\n*     *\n * * *\n*     *\n*     *\n*     *\n * * *\n";
         break;
         case 9:
             cout <<" * * *\n*     *\n*     *\n*     *\n * * *\n      *\n      *\n      *\n * * *\n";
