b575d225028ad47257d69753b54fbc42ceb75ae6
Evan <evanzhang1028@hotmail.com>
2020-11-05 22:37:51 -0500

    Fix ccc97s4 solution

--- CCC/ccc97s4.py
+++ CCC/ccc97s4.py
@@ -1,15 +1,17 @@
 import sys
 n = int(input())
-v = sys.stdin.read().split('\n\n')
+v = sys.stdin.read().strip().split('\n\n')
 for x in v:
     dic = {}
     cnt = 1
     for a in x.split('\n'):
+        line = []
         for b in a.split():
             if b in dic:
-                print(dic[b], end=" ")
+                line.append(dic[b])
             else:
-                print(b, end=" ")
+                line.append(b)
                 dic[b] = cnt
                 cnt += 1
-        print()
+        print(' '.join(map(str, line)))
+    print()
