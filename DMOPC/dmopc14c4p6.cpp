57da78a253cff7cd61061d4db1ce2b4af340cae0
Evan <evanzhang1028@hotmail.com>
2021-05-02 12:32:41 -0400

    dmopc14c4p6: update with cleaner recursive solution

--- DMOPC/dmopc14c4p6.cpp
+++ DMOPC/dmopc14c4p6.cpp
@@ -2,56 +2,34 @@
 
 using namespace std;
 
-vector<vector<int>> adj;
+vector<vector<int>> adj(500005);
+vector<vector<int>> dis(2, vector<int>(500005));
 
-void dfs(int u, int p, int diam, int &maxDiam, int &maxNode)
+void dfs(int u, int p, int d, int &mN, int &mD, int c)
 {
-    if (diam > maxDiam)
-        maxDiam = diam, maxNode = u;
+    if (d > mD)
+        mD = d, mN = u;
     for (auto &x : adj[u])
         if (x != p)
-            dfs(x, u, diam+1, maxDiam, maxNode);
+            dfs(x, u, d+1, mN, mD, c);
+    dis[c][u] = d;
 }
 
-vector<int> bfs(int src)
-{
-    vector<int> dist (adj.size(),1);
-    queue<pair<int,int>> buf;
-    buf.push(make_pair(src, src));
-    while (!buf.empty())
-    {
-        int u = buf.front().first, p = buf.front().second;
-        buf.pop();
-        for (auto &x : adj[u])
-        {
-            if (x != p)
-            {
-                dist[x] = dist[u]+1;
-                buf.push(make_pair(x, u));
-            }
-        }
-    }
-    return dist;
-}
-
-int main()
+int main() 
 {
     int n, a, b;
     scanf("%i", &n);
-    adj.resize(n);
     for (int x = 1; x < n; x++)
     {
         scanf("%i%i", &a, &b);
-        a--, b--;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    int max1 = 0, max2 = 0, n1, n2;
-    dfs(0, 0, 0, max1, n1);
-    dfs(n1, n1, 0, max2, n2);
-    vector<int> dis1 = bfs(n1);
-    vector<int> dis2 = bfs(n2);
-    for (int x = 0; x < n; x++)
-        printf("%i\n", max(dis1[x], dis2[x]));
+    int mN = 0, mD = 0;
+    dfs(1, 1, 0, mN, mD, 0);
+    dfs(mN, mN, 1, mN, mD=0, 0);
+    dfs(mN, mN, 1, mN, mD=0, 1);
+    for (int x = 1; x <= n; x++)
+        printf("%i\n", max(dis[0][x], dis[1][x]));
     return 0;
 }
